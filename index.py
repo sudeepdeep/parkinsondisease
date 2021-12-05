b63394e2b9a74a8afd10c863ee37889ac700045f
developer deepu <65069470+sudeepdeep@users.noreply.github.com>
2021-12-05 15:52:57 +0530

    Update index.py

--- index.py
+++ index.py
@@ -35,20 +35,23 @@ def home():
             model = pkl.load(open('parkinsonmodel.sav', 'rb'))
 
             res = model.predict([[val1, val2, val3, val4, val5, val6, val7, val8, val9, val10, val11, val12, val13, val14, val15, val16, val17, val18, val19, val20, val21, val22]])[0]
-            print(type(res))
+        
             if(res == '1'):  
-
-                msg = "Parkinson Disease is Present"
+                type = "red"
+                msg = "Parkinson Disease Detected"
             elif(res == '0'):  
+                type = "green" 
                 msg = "Hurray.. You're Not Infected With Parkinson"
 
-            return render_template('home.html', message = msg)
+            return render_template('home.html', message = msg, typing = type)
 
         else:
             return render_template('home.html')
 
     return render_template('home.html')
 
-
+@app.route('/treatment')
+def treatment():
+    return render_template('treatment.html')
 if __name__ == '__main__':
     app.run(debug=True)
